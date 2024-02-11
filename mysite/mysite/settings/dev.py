f7b3a350913e91403c8fa1a130a7538ad9c32636
Raphael Lebailly <raphael.lebailly@hotmail.com>
2024-02-11 17:58:54 +0100

    created default project

---
+++ mysite/mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-qm%!-1+4ue*t^9-a)200t@g(lg*$5(3n2a2yt#hfl2l6a7bqt2"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
