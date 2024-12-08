8f38fc5bee5032e60478ed09940fbc240d6ec7b5
Raphael Lebailly <raphael.lebailly@hotmail.com>
2024-12-08 13:55:37 +0100

    separating dev and production

--- mysite/mysite/settings/dev.py
+++ mysite/mysite/settings/dev.py
@@ -4,7 +4,7 @@ from .base import *
 DEBUG = True
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-qm%!-1+4ue*t^9-a)200t@g(lg*$5(3n2a2yt#hfl2l6a7bqt2"
+# SECRET_KEY = "django-insecure-qm%!-1+4ue*t^9-a)200t@g(lg*$5(3n2a2yt#hfl2l6a7bqt2"
 
 # SECURITY WARNING: define the correct hosts in production!
 ALLOWED_HOSTS = ["*"]
